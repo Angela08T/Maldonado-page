09502572e130ccc5cea2d5cede8955ba5b3157f6
Joel Hidalgo Perez <jhidalgop@intranet.mdsjl>
2026-07-14 10:06:32 -0500

    se agrego nuevas fotos a la galeria

--- src/components/Galeria/Galeria.jsx
+++ src/components/Galeria/Galeria.jsx
@@ -5,12 +5,18 @@ import g1 from '../../assets/galeria1.jpg'
 import g2 from '../../assets/galeria2.jpg'
 import g3 from '../../assets/galeria3.jpg'
 import g4 from '../../assets/galeria4.jpg'
+import g5 from '../../assets/galeria5.jpeg'
+import g6 from '../../assets/galeria6.jpeg'
+import g7 from '../../assets/galeria7.jpeg'
 
 const images = [
   { thumb: g1, full: g1, alt: 'Galería 1' },
   { thumb: g2, full: g2, alt: 'Galería 2' },
   { thumb: g3, full: g3, alt: 'Galería 3' },
   { thumb: g4, full: g4, alt: 'Galería 4' },
+  { thumb: g5, full: g5, alt: 'Galería 5' },
+  { thumb: g6, full: g6, alt: 'Galería 6' },
+  { thumb: g7, full: g7, alt: 'Galería 7' },
 ]
 
 export default function Galeria() {
